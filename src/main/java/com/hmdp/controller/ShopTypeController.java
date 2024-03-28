f2871d4661ac3ea4654dd6e5cdbe6c2fad0b3c41
branchtree <branchtree97@outlook.com>
2024-03-28 10:46:12 +0800

    店铺分类查询及缓存

--- src/main/java/com/hmdp/controller/ShopTypeController.java
+++ src/main/java/com/hmdp/controller/ShopTypeController.java
@@ -25,10 +25,13 @@ public class ShopTypeController {
     @Resource
     private IShopTypeService typeService;
 
+    /**
+     * 查询商铺分类
+     * @return
+     */
     @GetMapping("list")
     public Result queryTypeList() {
-        List<ShopType> typeList = typeService
-                .query().orderByAsc("sort").list();
-        return Result.ok(typeList);
+
+        return typeService.typeList();
     }
 }
--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import java.util.List;
+
 
 /**
  * <p>
@@ -17,4 +26,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 查询商品分类
+     * @return
+     */
+    @Override
+    public Result typeList() {
+        String key="cache:shopType";
+        //查找reidis中的商品分类数据
+        String shopTypeJson = stringRedisTemplate.opsForValue().get(key);
+
+        //如果存在直接返回
+        if(StrUtil.isNotBlank(shopTypeJson)){
+            List<ShopType> shopTypeList = JSONUtil.toList(shopTypeJson, ShopType.class);
+            return Result.ok(shopTypeList);
+        }
+
+        //如果不存在，查找mysql数据库
+        List<ShopType> shopTypeList = query().orderByAsc("sort").list();
+        if(shopTypeList==null){
+            return Result.fail("分类不存在");
+        }
+
+        stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shopTypeList));
+        return Result.ok(shopTypeList);
+
+    }
 }
