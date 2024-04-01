80e8c110b1c1514923d0f1090415643ff940741f
branchtree <branchtree97@outlook.com>
2024-04-01 20:18:42 +0800

    生成下单时的id

---
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,41 @@
+package com.hmdp.utils;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class RedisIdWorker {
+    /**
+     * 开始时间戳
+     */
+    private static final long BEGIN_TIMESTAMP = 1640995200L;
+    /**
+     * 序列号的位数
+     */
+    private static final int COUNT_BITS = 32;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    public RedisIdWorker(StringRedisTemplate stringRedisTemplate){
+        this.stringRedisTemplate=stringRedisTemplate;
+    }
+    public long nextId(String keyPrefix){
+        //生成时间戳
+        LocalDateTime now=LocalDateTime.now();
+        long nowSeconds=now.toEpochSecond(ZoneOffset.UTC);
+        long timestamp=nowSeconds-BEGIN_TIMESTAMP;
+        //生成序列号
+        //获取当前日期，精确到天
+        String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:yy"));
+        //自增长
+        long count=stringRedisTemplate.opsForValue().increment("icr:"+keyPrefix+":"+date);
+        return timestamp<<COUNT_BITS|count;
+    }
+}
